7d0af7ca45b3aec6f6488245efaca138739eaa88
anafsilva <anacarolinq.ng@gmail.com>
2022-02-08 13:55:55 -0300

    feat(components/MovieRow): created display list to movies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from "react"
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
-export default () =>{
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pegando a lista total
+
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, [])
+
+
   return (
-    <div>
-      Olá Mundo
+    <div className="page">
+
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <div>
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          </div>
+        ))}
+      </section>
     </div>
-    )
+  )
 }
