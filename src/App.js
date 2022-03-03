c1144fe03274847e63de47819f91b9dd71905a84
anafsilva <anacarolinq.ng@gmail.com>
2022-03-03 15:41:15 -0300

    feat: created all pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
+
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
-
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
 
   useEffect(() => {
     const loadAll = async () => {
       //Pegando a lista total
-
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //Pegando o Featured
+
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosem = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosem];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo); 
+
+
     }
 
     loadAll();
   }, [])
 
+  useEffect(() =>{
+    const scrollListener = () =>{
+      if(window.scrollY > 600){
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return() =>{
+      window.removeEventListener('scroll', scrollListener);
+    }
+
+  }, []);
 
   return (
     <div className="page">
+
+      <Header black={blackHeader} />
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
 
       <section className="lists">
         {movieList.map((item, key)=>(
@@ -30,6 +66,11 @@ export default () => {
           </div>
         ))}
       </section>
+
+      <footer>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos do site Themoviedb.org
+      </footer>
     </div>
   )
 }
